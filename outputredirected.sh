6bc3cdc266f43af3560910c9b9f85548f9b900fb
Tauksik <84122841+Tauksik@users.noreply.github.com>
2023-01-10 12:07:29 +0530

    Create outputredirected.sh

---
+++ outputredirected.sh
@@ -0,0 +1,16 @@
+#!/bin/sh/
+# redirect output to a different file
+answer=y
+while [ "$answer" = "y" ]
+do 
+	echo "enter code and description: " >/dev/tty
+	read code description
+	echo "$code | $description" >> newfile
+	echo "enter any more (y/n)?: \c" >/dev/tty
+	read anymore
+	case $anymore in 
+		y*|Y*) answer=y ;;
+		n*|N*) answer=n ;;
+		*) answer=y ;;
+	esac
+done
